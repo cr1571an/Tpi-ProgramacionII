1f7a96647c357876e5346f258412d8761ce31f09
Cristian Sanchez <sanchez.cn@hotmail.com>
2025-11-07 17:09:41 -0300

    Refactor utils.cpp and utils.h: reorganize includes and improve code formatting for consistency

--- utils.cpp
+++ utils.cpp
@@ -1,21 +1,29 @@
-#include <iostream>
 #include "utils.h"
 
-std::string cargarCadena(){
-    std::string texto;
-    if(std::cin.peek() == '\n'){
-        std::cin.ignore();
-    }
-    std::getline(std::cin, texto);
-    return texto;
+#include <iostream>
+#include "Fecha.h"
+
+std::string cargarCadena() {
+  std::string texto;
+  if (std::cin.peek() == '\n') {
+    std::cin.ignore();
+  }
+  std::getline(std::cin, texto);
+  return texto;
 }
 
-int buscarMaximo(int vec[], int cant){
-    int indexMax = 0;
-    for(int i=1; i<cant; i++){
-        if(vec[i] > vec[indexMax]){
-            indexMax = i;
-        }
+int buscarMaximo(int vec[], int cant) {
+  int indexMax = 0;
+  for (int i = 1; i < cant; i++) {
+    if (vec[i] > vec[indexMax]) {
+      indexMax = i;
     }
-    return indexMax;
+  }
+  return indexMax;
+}
+Fecha procesarFecha(std::string fecha) {
+  int anio = std::stoi(fecha.substr(0, 4));
+  int mes = std::stoi(fecha.substr(5, 2));
+  int dia = std::stoi(fecha.substr(8, 2));
+  return Fecha(dia, mes, anio);
 }
