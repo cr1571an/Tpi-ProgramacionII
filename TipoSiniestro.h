c2d132e24566b6ca4097e93207860841d464425a
Cristian Sanchez <sanchez.cn@hotmail.com>
2025-11-16 17:38:56 -0300

    Add TipoSiniestro class with constructors and accessor methods

---
+++ TipoSiniestro.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+class TipoSiniestro {
+ public:
+  TipoSiniestro();
+  TipoSiniestro(int id, const std::string descripcion, bool eliminado);
+
+  int getId();
+  std::string getDescripcion();
+  bool getEliminado();
+
+  void setId(int id);
+  void setDescripcion(std::string descripcion);
+  void setEliminado(bool eliminado);
+
+ private:
+  int _id;
+  char _descripcion[15];
+  bool _eliminado;
+};
