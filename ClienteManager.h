567891f4e260a7c17d8215785cc35064ef0a4b42
Cristian Sanchez <c.sanchezmdp@gmail.com>
2025-12-23 11:21:55 -0300

    Encapsular clases.

--- ClienteManager.h
+++ ClienteManager.h
@@ -15,22 +15,23 @@ public:
     void mostrar();
     void mostrarDatosDeClienteDNI();
     void mostrarDatosDeClienteID(int idCliente);
+    int posClientePorDNI(std::string dni);
+    bool eliminar(int idCliente);
     void modificarTelefono(int idCliente);
     void modificarCorreo(int idCliente);
     void modificarFechaNacimiento(int idCliente);
     void modificarLocalidad(int idCliente);
     void modificarPartido(int idCliente);
+    bool recuperar(int idCliente);
+    void historialPagosPorCliente();
 
-    int posClientePorDNI(std::string dni);
+private:
 
     bool validarEdad(Fecha fechaNacimiento);
     bool telefonoDisponible(std::string telefono);
     bool correoDisponible(std::string correo);
     bool verificarRegistroPorDNI(std::string dni);
-    bool eliminar(int idCliente);
-    bool recuperar(int idCliente);
     void mostrarLista(Cliente cliente, bool eliminado);
-
     int hayClientesReg();
     int hayPolizasReg();
     int hayPagosVencReg();
@@ -38,10 +39,7 @@ public:
     int buscarClienteParaHistorial();
     bool esPagoDeCliente(int idxPago, int idCliente);
     int contarPagosDeCliente(int idCliente);
-    void historialPagosPorCliente();
 
-
-private:
     ClientesArchivo _clientesArchivo;
     VehiculosArchivo _vehiculosArchivo;
     PolizaArchivo _polizasArchivo;
--- VehiculoManager.h
+++ VehiculoManager.h
@@ -11,25 +11,23 @@ class VehiculoManager {
         void mostrar();
         void eliminar(int idVehiculo);
         bool recuperar(int idVehiculo);
+        int buscarIdPorPatente(std::string patente);
+        int mostrarYContarVehiculosDeCliente(int idClienteBuscado);
+        void mostrarLista(Vehiculo vehiculo);
         void modificarPatente(int id);
         void modificarUso(int id);
         void modificarCategoria(int id);
         void modificarNumChasis(int id);
         void modificarNumMotor(int id);
         void modificarAnio(int id);
+    private:
+    
         void mostrarPantalla(Cliente cli, int id, std::string marca, std::string modelo, std::string patente, int anio, std::string chasis, std::string motor, std::string uso = "", std::string categoria = "");
-
         bool estadoCliente(int idCliente);
-        int buscarIdPorPatente(std::string patente);
-        int posVehiculoPorClienteId(int idCliente);
-        int mostrarYContarVehiculosDeCliente(int idClienteBuscado);
-
-        std::string nombreApellidoClinete(int idCliente);
+        int posVehiculoPorClienteId(int idCliente);        
         bool buscarNumMotor(std::string numMotor);
         bool buscarNumChasis(std::string numChasis);
-        void mostrarLista(Vehiculo vehiculo);
 
-    private:
         VehiculosArchivo _vehiculosArchivo;
         ClientesArchivo _clientesArchivo;
 };
